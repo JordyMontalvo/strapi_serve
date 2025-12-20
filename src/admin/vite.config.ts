5760aad6c2766df612b59565813dfaa6115cd689
Ubuntu <bibliotecas@POCBIBLIOTECA.iwm3q51xgvmuvo1mxt2anhqpmd.sclx.internal.cloudapp.net>
2025-12-20 18:58:12 +0000

    configuracion para ngnix

---
+++ src/admin/vite.config.ts
@@ -0,0 +1,20 @@
+import { mergeConfig, type UserConfig } from 'vite';
+
+export default (config: UserConfig) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: [
+        'cmsbiblioteca.aiep.cl',
+        '68.211.112.39',
+        'localhost'
+      ],
+    },
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  });
+};
