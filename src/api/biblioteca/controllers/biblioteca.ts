0cc77f8392cc30736dbb40e215d7e83377f8336c
Ubuntu <bibliotecas@POCBIBLIOTECA.iwm3q51xgvmuvo1mxt2anhqpmd.sclx.internal.cloudapp.net>
2026-02-21 17:49:02 +0000

    api biblioteca

---
+++ src/api/biblioteca/controllers/biblioteca.ts
@@ -0,0 +1,7 @@
+/**
+ * biblioteca controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::biblioteca.biblioteca');
---
+++ src/api/biblioteca/services/biblioteca.ts
@@ -0,0 +1,7 @@
+/**
+ * biblioteca service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::biblioteca.biblioteca');
